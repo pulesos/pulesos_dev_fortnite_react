3985a3e85318136cadbae519f4cddaf5fecd69b1
Eugene <evgen7661@gmail.com>
2022-04-07 20:39:12 +0300

    added gh-pagesрп

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,6 @@ const Footer = () => {
             <div className="footer-copyright">
             <div className="container">
                 © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
           </div>
         </footer>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,9 +7,7 @@ const Header = (props) => {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">Fortnite Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="badges.html">Components</a></li>
-                    <li><a href="collapsible.html">JavaScript</a></li>
+                    <li><a href="https://github.com/pulesos/pulesos_dev_fortnite_react">My Repo</a></li>
                 </ul>
             </div>
         </nav>
